a8decf5142b8ba36de94cdae77f5074e310dbe2f
agoston <agoston@geogebra.org>
2020-05-01 13:10:45 +0200

    MOW-1029: don't load image into DOM

--- web/src/main/java/org/geogebra/web/html5/safeimage/ConvertToCanvas.java
+++ web/src/main/java/org/geogebra/web/html5/safeimage/ConvertToCanvas.java
@@ -1,14 +1,12 @@
 package org.geogebra.web.html5.safeimage;
 
+import com.google.gwt.user.client.Element;
 import org.geogebra.common.util.FileExtensions;
 import org.geogebra.common.util.StringUtil;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.event.dom.client.LoadEvent;
-import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.RootPanel;
 
 public class ConvertToCanvas implements ImagePreprocessor {
 	private final Canvas canvas;
@@ -26,11 +24,10 @@ public class ConvertToCanvas implements ImagePreprocessor {
 	@Override
 	public void process(final ImageFile imageFile, final SafeImageProvider provider) {
 		final Image image = new Image();
-		RootPanel.get().add(image);
-		image.setUrl(imageFile.getContent());
-		image.addLoadHandler(new LoadHandler() {
+		//TODO: fix on cleanup events
+		addLoadEventListener(image.getElement(), new Runnable() {
 			@Override
-			public void onLoad(LoadEvent event) {
+			public void run() {
 				drawImageToCanvas(image);
 				String fileName = StringUtil.changeFileExtension(imageFile.getFileName(),
 						FileExtensions.PNG);
@@ -38,7 +35,14 @@ public class ConvertToCanvas implements ImagePreprocessor {
 				provider.onReady(new ImageFile(fileName, canvas.toDataUrl()));
 			}
 		});
+		image.setUrl(imageFile.getContent());
 	}
+
+	private native void addLoadEventListener(Element element, Runnable runnable) /*-{
+		element.addEventListener('load', function() {
+		    runnable.@java.lang.Runnable::run()();
+		});
+	}-*/;
 
 	private void drawImageToCanvas(Image image)  {
 		canvas.setCoordinateSpaceWidth(image.getWidth());
